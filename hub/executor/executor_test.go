f5b838958e526cbfa42f9ddbbade8938e03b2ca7
Luoxin <luoxin.ttt@gmail.com>
2022-04-06 21:43:41 +0800

    test

--- hub/executor/executor_test.go
+++ hub/executor/executor_test.go
@@ -15,7 +15,7 @@ func TestExecutor(t *testing.T) {
 		return
 	}
 
-	err = ex.AddNodeByV2rayLink("trojan://E73EAC23-83C8-AC21-E491-A6F3FC7C1FE0@shdata1.ourdvsss.xyz:12122?security=tls&sni=douyincdn.com&type=tcp&headerType=none#%f0%9f%87%ba%f0%9f%87%b8+%e7%be%8e%e5%9b%bd02")
+	err = ex.AddNodeByV2rayLink("trojan://nykzeZ-virza6-kagmyf@teoeu.com:443#ShareCentre")
 	if err != nil {
 		log.Errorf("err:%v", err)
 		return
